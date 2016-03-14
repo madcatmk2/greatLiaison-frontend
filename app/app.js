d7fd81d730d342d6e41db32c4cc9288437d5ef87
Herman Ng <heymanhn@gmail.com>
2016-03-14 03:22:44 -0700

    Display subset of GLL listings with angular

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('gllApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'gllApp.gll'
+]);
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.otherwise({redirectTo: '/gll'});
+// }]);
