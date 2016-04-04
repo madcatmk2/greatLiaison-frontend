95fcaa5bedab7d533785345ff3cf7ec3ba042d83
Herman Ng <heymanhn@gmail.com>
2016-04-04 01:09:38 -0700

    Set up web server with stubbed API response

---
+++ README.md
@@ -0,0 +1,20 @@
+# G&L Frontend server
+
+This server is used to launch the G&L frontend site and provide stubbed data via an API.
+
+## To use
+
+```sh
+npm install
+node server.js
+```
+
+Then visit <http://localhost:3000/>.
+
+## Changing the port
+
+You can change the port number by setting the `$PORT` environment variable before invoking any of the scripts above, e.g.,
+
+```sh
+PORT=3001 node server.js
+```
---
+++ server.js
@@ -0,0 +1,40 @@
+/*
+ * Simple web server for G&L website
+ */
+
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+var GLL_PRODUCTS = path.join(__dirname, 'json/gll.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    next();
+});
+
+app.get('/api/products', function(req, res) {
+  fs.readFile(GLL_PRODUCTS, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
