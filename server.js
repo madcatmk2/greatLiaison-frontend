f7674e0699ed809c5d3cc50cbf3f88d5cf8bc907
Herman Ng <heymanhn@gmail.com>
2016-05-16 17:12:29 -0700

    Reinsert app.get() middleware for react-router
    The middleware forwards all routes to the react-router logic.

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ var app = express();
 app.set('port', (process.env.PORT || 8090));
 app.use(express.static('public'));
 
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+// send all requests to index.html so browserHistory in React Router works
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 app.server = http.createServer(app);
